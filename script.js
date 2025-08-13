3fd93f92d6a6fb4c25388013d16328fc713df23d
Foziljon <boltaboyevafotima1@gmail.com>
2025-08-13 18:54:18 +0500

    v2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-  const navMenu = document.getElementById("navMenu");
-  navMenu.addEventListener("change", function () {
-    const url = this.value;
-    if (url !== "#") {
-      window.location.href = url;
-    }
-  });
+const navMenu = document.getElementById("navMenu");
+navMenu.addEventListener("change", function () {
+  const url = this.value;
+  if (url !== "#") {
+    window.open(url, "_blank"); 
+    this.value = "#"; 
+  }
+});
+
 
   document.addEventListener("DOMContentLoaded", () => {
     const focusInp = document.getElementById("email");
